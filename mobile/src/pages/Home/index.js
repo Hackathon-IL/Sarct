1532424d2a6460536fd0177b8dde555008b84eb4
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2021-04-14 23:37:23 -0300

    add home page

---
+++ mobile/src/pages/Home/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {Container, Item} from './styles';
+import bombeiro from '../../assets/bo.jpg';
+import medico from '../../assets/doutor.jpg';
+import police from '../../assets/police.jpg';
+import banco from '../../assets/banco.png';
+import internado from '../../assets/internado.jpg';
+import familia from '../../assets/familia.png';
+function Home(){
+
+    const data = [
+    {
+        image: medico,
+        name: 'medico'
+    },
+    {
+        image: bombeiro,
+        name: 'bombeiro'
+    },
+    {
+        image: police,
+        name: 'police'
+    },
+    {
+        image: banco,
+        name: 'banco'
+    },
+    {
+        image: internado,
+        name: 'internado'
+    },
+    {
+        image: familia,
+        name: 'familia'
+    },
+
+
+    ]
+    return <Container
+        data={data}
+        numColumns={2}
+        flexBasis={0}
+        keyExtractor={item => String(item)}
+        renderItem={({item})=>(
+            <Item source={item.image}/>
+        )}
+    />;
+}
+
+export default Home;
